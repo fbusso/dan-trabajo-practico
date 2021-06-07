42fcbe089abd7e5048268dd6b2eeb99457ddbb5d
Francisco Busso <francisco.busso@outlook.com>
2021-06-07 00:37:57 -0300

    Merge

--- src/pedido/src/main/java/com/dan/pedido/configuracion/ConfiguracionWebClient.java
+++ src/pedido/src/main/java/com/dan/pedido/configuracion/ConfiguracionWebClient.java
@@ -3,12 +3,13 @@ package com.dan.pedido.configuracion;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClient.Builder;
 
 @Configuration
 public class ConfiguracionWebClient {
 
     @Bean
-    WebClient webClient(WebClient.Builder webClientBuilder) {
+    WebClient webClient(Builder webClientBuilder) {
         return webClientBuilder.build();
     }
 }
