128b42f41c4da0cc549da25bcccb3f3b3bb8fb0f
Bode Matias <32490307+BodeMatias@users.noreply.github.com>
2021-08-10 19:55:19 -0300

    Deshabilitado csrf y agregado Cross origin (#10)

--- src/usuario/src/main/java/com/dan/usuario/configuracion/ConfiguracionKeycloak.java
+++ src/usuario/src/main/java/com/dan/usuario/configuracion/ConfiguracionKeycloak.java
@@ -36,6 +36,7 @@ public class ConfiguracionKeycloak extends KeycloakWebSecurityConfigurerAdapter 
     {
         super.configure(http);
         http
+                .csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/api/**").hasRole("USUARIO")
                 .and()
