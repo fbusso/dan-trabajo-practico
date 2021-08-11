22d6b9c3c573e20a7653c87696201493343c96f4
Matias <bode.matias@hotmail.com>
2021-08-11 18:27:25 -0300

    Arreglados problemas de cors

--- src/usuario/src/main/java/com/dan/usuario/controlador/ObraControlador.java
+++ src/usuario/src/main/java/com/dan/usuario/controlador/ObraControlador.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "*", methods= {RequestMethod.GET,RequestMethod.POST})
+@CrossOrigin(origins = "*")
 @RequestMapping("/api/obras")
 public class ObraControlador {
 
