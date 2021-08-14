026a64a079a92302b7fd734a8219e003859aba86
Francisco Busso <francisco.busso@outlook.com>
2021-08-14 19:11:22 -0300

    Agregada parametrización de endpoints

--- src/usuario/src/main/java/com/dan/usuario/servicio/impl/PedidoServicioImpl.java
+++ src/usuario/src/main/java/com/dan/usuario/servicio/impl/PedidoServicioImpl.java
@@ -4,6 +4,7 @@ import com.dan.usuario.dto.PedidoDto;
 import com.dan.usuario.excepcion.ReglaDeNegociosExcepcion;
 import com.dan.usuario.servicio.HttpServicio;
 import com.dan.usuario.servicio.PedidoServicio;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.Arrays;
@@ -14,14 +15,15 @@ public class PedidoServicioImpl implements PedidoServicio {
 
     private final HttpServicio httpServicio;
 
+    @Value("${pedidos.obtener-pedidos-por-cliente-id}")
+    private String URL;
+
     public PedidoServicioImpl(HttpServicio httpServicio) {
         this.httpServicio = httpServicio;
     }
 
     @Override
     public List<PedidoDto> obtenerPorClienteId(Integer id) throws ReglaDeNegociosExcepcion {
-        // TODO: Parametrización de URLs
-        final String URL = "http://localhost:9002/api/pedido/cliente/{id}";
         return Arrays.asList(httpServicio.get(PedidoDto[].class, URL, id));
     }
 }
