78be33568281c7d675142825ace74a7bc93737d0
Francisco Busso <francisco.busso@outlook.com>
2021-08-15 19:09:52 -0300

    Agregada recepcion de pedidos en cola de mensajes

--- src/producto/src/main/java/com/dan/producto/servicio/ColaMensajesServicio.java
+++ src/producto/src/main/java/com/dan/producto/servicio/ColaMensajesServicio.java
@@ -1,4 +1,8 @@
 package com.dan.producto.servicio;
 
+import com.dan.producto.dto.PedidoDto;
+
 public interface ColaMensajesServicio {
+
+    void mensajeRecibido(PedidoDto pedido);
 }
--- src/producto/src/main/java/com/dan/producto/servicio/impl/ColaMensajesServicioImpl.java
+++ src/producto/src/main/java/com/dan/producto/servicio/impl/ColaMensajesServicioImpl.java
@@ -1,16 +1,22 @@
 package com.dan.producto.servicio.impl;
 
+import com.dan.producto.dto.PedidoDto;
 import com.dan.producto.servicio.ColaMensajesServicio;
+import com.dan.producto.servicio.MaterialServicio;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ColaMensajesServicioImpl implements ColaMensajesServicio {
 
+    private final MaterialServicio materialServicio;
+
+    public ColaMensajesServicioImpl(MaterialServicio materialServicio) {
+        this.materialServicio = materialServicio;
+    }
 
     @RabbitListener(queues = "COLA_PEDIDOS")
-    // TODO: Implementación. Test
-    public void mensajeRecibido(Object pedido) {
-        System.out.println("Mensaje recibido desde RabbitMQ: " + pedido);
+    public void mensajeRecibido(PedidoDto pedido) {
+        materialServicio.registrarMovimiento(pedido);
     }
 }
