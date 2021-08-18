65d0d8c28b9ac1325e75caaee3a0790efe6161c4
Francisco Busso <francisco.busso@outlook.com>
2021-08-18 18:39:06 -0300

    Corregido compose y eliminado script innecesario

--- build.ps1
+++ build.ps1
@@ -1,3 +1,6 @@
+# Levanta el servicio de base de datos para buildear las imágenes
+docker-compose up -d database
+
 # Build de imagen de Microservicio de BCRA
 cd src/bcra
 ./mvnw spring-boot:build-image -DskipTests=true
@@ -22,3 +25,6 @@ cd ../../
 cd src/usuario
 ./mvnw spring-boot:build-image -DskipTests=true
 cd ../../
+
+# Baja del servicio de base de datos
+docker-compose up down database
--- build.sh
+++ build.sh
@@ -1,24 +1,30 @@
+# Levanta el servicio de base de datos para buildear las imágenes
+docker-compose up -d database
+
 # Build de imagen de Microservicio de BCRA
 cd src/bcra
-mvn spring-boot:build-image -DskipTests=true
+mvn spring-boot:build-image -DskipTests=true -Pdeploy
 cd ../../
 
 # Build de imagen de Microservicio de Cuenta Corriente
 cd src/cuenta
-mvn spring-boot:build-image -DskipTests=true
+mvn spring-boot:build-image -DskipTests=true -Pdeploy
 cd ../../
 
 # Build de imagen de Microservicio de Pedido
 cd src/pedido
-mvn spring-boot:build-image -DskipTests=true
+mvn spring-boot:build-image -DskipTests=true -Pdeploy
 cd ../../
 
 # Build de imagen de Microservicio de Producto
 cd src/producto
-mvn spring-boot:build-image -DskipTests=true
+mvn spring-boot:build-image -DskipTests=true -Pdeploy
 cd ../../
 
 # Build de imagen de Microservicio de Usuario
 cd src/usuario
-mvn spring-boot:build-image -DskipTests=true
+mvn spring-boot:build-image -DskipTests=true -Pdeploy
 cd ../../
+
+# Baja del servicio de base de datos
+docker-compose up down database
