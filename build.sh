09431270a33e5e886d21335d849b578c0653ae9b
Francisco Busso <francisco.busso@outlook.com>
2021-08-18 23:57:34 -0300

    Ajuste en entorno, scripts de build y parametrización

--- build.sh
+++ build.sh
@@ -8,22 +8,22 @@ cd ../../
 
 # Build de imagen de Microservicio de Cuenta Corriente
 cd src/cuenta
-mvn spring-boot:build-image -DskipTests=true -Pdeploy
+mvn spring-boot:build-image -DskipTests=true
 cd ../../
 
 # Build de imagen de Microservicio de Pedido
 cd src/pedido
-mvn spring-boot:build-image -DskipTests=true -Pdeploy
+mvn spring-boot:build-image -DskipTests=true
 cd ../../
 
 # Build de imagen de Microservicio de Producto
 cd src/producto
-mvn spring-boot:build-image -DskipTests=true -Pdeploy
+mvn spring-boot:build-image -DskipTests=true
 cd ../../
 
 # Build de imagen de Microservicio de Usuario
 cd src/usuario
-mvn spring-boot:build-image -DskipTests=true -Pdeploy
+mvn spring-boot:build-image -DskipTests=true
 cd ../../
 
 # Build de imagen de Microservicio de Proxy
